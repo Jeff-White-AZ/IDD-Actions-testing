b98ecbc7e43e9968325a22be0d7972887c075c7c
Jeff White <160061544+Jeff-White-AZ@users.noreply.github.com>
2025-04-14 16:29:48 -0700

    Update ICASA_DD_csv_to_xlsx.R

--- R/ICASA_DD_csv_to_xlsx.R
+++ R/ICASA_DD_csv_to_xlsx.R
@@ -1,62 +1,118 @@
-name: Reassemble CSVs and Upload XLSX
+# 1. Introduction ==============================================================
+#' Converts a series of csv files to xlsx format for the ICASA DD.
+#' 
+print "starting script"
+# 2. Set working directory to script location. Define file name and paths.======
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-on:
-  push:
-    branches:
-      - main
+library(openxlsx2)
 
-permissions:
-  actions: read
-  contents: read
+path_for_xlsx <- file.path(file_name)
+subfolder_for_csv <- "Data"
 
-jobs:
-  build:
-    runs-on: ubuntu-latest
+dictionary_sheets <- c("ReadMe", "Metadata", "Management_info", "Soils_data", "Weather_data", 
+    "Measured_data", "Metadata_codes", "Management_codes", "Crop_codes",
+    "Pest_codes", "Other_codes", "Glossary")        
+  
+# 3. Load the target wb and obtain sheet names =================================
+print("Creating workbook")
+# Create a new workbook
+wb <- wb_workbook()
 
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v3
+# 4. Loop through CSV files and add each as a worksheet ========================
+for (file in dictionary_sheets) {
+  # Read CSV into data frame
+  csv_file <- paste0(file, ".csv")
+  csv_file_path <- file.path(subfolder_for_csv, csv_file)
+  df <- read.csv(csv_file_path, stringsAsFactors = FALSE)
+  
+  # Create a sheet name from the file name (remove folder and extension)
+  # Add worksheet and write data
+  wb$add_worksheet(sheet = file)
+  wb$add_data(sheet = file, x = df, na.strings = "")
+  
+}
 
-      - name: Set up R
-        uses: r-lib/actions/setup-r@v2
-        with:
-          r-version: '4.4.2'  # Specify the R version you need
+# 5. Add further formatting as needed. =========================================
+# 5.1 Freeze at top line =======================================================
+for (file in dictionary_sheets) {
+  wb$freeze_pane(sheet = file, first_active_row = 2)  
+}
 
-      - name: Install R dependencies
-        run: >
-          R -e "install.packages('openxlsx2')"  # Add other packages if necessary
+# 5.2 Set column widths and text wrapping in ReadMe ============================
 
-      - name: Prepare output directory
-        run: |
-          mkdir -p Data/output
+for (file in dictionary_sheets) {
+  print(file)
+  if (file == "ReadMe") {
+    wb$set_col_widths(sheet = file, cols = c(1, 2), widths = c(90, 20))
+    # Apply wrap style to the column
+    # wb$set_row_heights(sheet = file, rows = 1:20, heights = 40)
+    wb$add_cell_style(sheet=file, dims = "A1:A20", 
+                      horizontal = "left", vertical = "top", wrap_text = TRUE)  
+    wb$add_font(sheet = file, dims = "A1:A1", bold = TRUE)
+    wb$add_font(sheet = file, dims = "A6:A6", bold = TRUE)
+  } else {
+    #wb$set_col_widths(sheet = file, cols = 1:22, widths = "auto")
+    wb$add_font(sheet = file, dims = "A1:V1", bold = TRUE)
+  }
+}
 
-      - name: List files and directories before running R script
-        run: |
-          echo "Listing all files and directories recursively:"
-          ls -lR
-          echo "Checking the contents of the root directory:"
-          ls -lR .
-          echo "Checking the contents of the R directory:"
-          ls -lR R
-          echo "Checking the contents of the Data directory:"
-          ls -l Data
-          echo "Checking the contents of the Data/output directory:"
-          ls -l Data/output
+# 5.2 Set column widths and text wrapping in sheets for data ===================
+data_sheets <- c("Metadata", "Management_info", "Soils_data", "Weather_data", 
+                  "Measured_data")
+col_widths <- rep(12, 8)
+for (file in data_sheets) {
+  wb$set_col_widths(sheet = file, cols = c(1:18), widths = c(22,18,18,14,14,60,12,12,15,15,col_widths))
+  wb$add_cell_style(sheet=file, dims = "F1:F100", 
+                    horizontal = "left", vertical = "top", wrap_text = TRUE)  
+  }
 
-      - name: Run R script to reassemble CSVs
-        run: Rscript R/ICASA_DD_csv_to_xlsx.R
+# 5.3 Set column widths and text wrapping in sheet for Metadata_codes ==========
+file <- "Metadata_codes"
+  wb$set_col_widths(sheet = file, cols = c(1:5), widths = c(20,12,60,40,15))
+  wb$add_cell_style(sheet=file, dims = "C1:D100", 
+                   horizontal = "left", vertical = "top", wrap_text = TRUE)  
 
-      - name: Debug: List files post script
-        run: |
-          echo "Listing all files and directories recursively after script:"
-          ls -lR
-          echo "Checking the contents of Data/output directory:"
-          ls -l Data/output
-          echo "Checking the contents of Data directory:"
-          ls -l Data
+# 5.4 Set column widths and text wrapping in sheet for Management_codes ========
+file <- "Management_codes"
+  wb$set_col_widths(sheet = file, cols = c(1:7), widths = c(12,12,12,40,20,40,15))
+  wb$add_cell_style(sheet=file, dims = "D1:F600", 
+                    horizontal = "left", vertical = "top", wrap_text = TRUE)  
 
-      - name: Upload reassembled XLSX file
-        uses: actions/upload-artifact@v4
-        with:
-          name: reassembled-xlsx
-          path: Data/output/*.xlsx  # Adjust the path to the actual XLSX file location
+# 5.5 Set column widths and text wrapping in sheet for Crop_codes ========
+file <-"Crop_codes"
+  wb$set_col_widths(sheet = file, cols = c(1:21), 
+                    widths = c(20,12,30,30,30,30,30,12,12,12,15,12,12,12,12,12,12,12,12,40,25))
+  wb$add_cell_style(sheet=file, dims = "G1:G200", 
+                    horizontal = "left", vertical = "top", wrap_text = TRUE)  
+
+# 5.6 Set column widths and text wrapping in sheet for Pest_codes ========
+file <- "Pest_codes"
+  wb$set_col_widths(sheet = file, cols = c(1:9), widths = c(12,10,18,40,12,12,30,12,12))
+  wb$add_cell_style(sheet=file, dims = "D1:D100", 
+                    horizontal = "left", vertical = "top", wrap_text = TRUE)  
+
+# 5.7 Set column widths and text wrapping in sheet for Other_codes ========
+file <- "Other_codes"
+  wb$set_col_widths(sheet = file, cols = c(1:6), widths = c(15,12,12,40,30,15))
+  wb$add_cell_style(sheet=file, dims = "D1:D100", 
+                    horizontal = "left", vertical = "top", wrap_text = TRUE)  
+
+# 5.7 Set column widths and text wrapping in sheet for the Glossary ========
+  file <- "Glossary"
+  wb$set_col_widths(sheet = file, cols = c(1:5), widths = c(20,20,60,12,12))
+  wb$add_cell_style(sheet=file, dims = "C1:C200", 
+                    horizontal = "left", vertical = "top", wrap_text = TRUE)  
+  
+# 6. Save the final, formatted ICASA DD workbook ===============================
+# 6.1 Before saving select ReadMe and set cell A1 as active. ===================
+wb$set_selected(sheet = 1)  # Necessary to avoid multiple sheets being selected.
+wb$set_bookview(active_tab = 0, first_sheet = 0)
+
+# 6.2 Save the final file
+print("Saving workbook")
+saved_xlsx <- file.path("..", "Combined_CSVs.xlsx")
+wb_save(wb, saved_xlsx)
+
+# 5. End script with message giving location of the CSV files. =================
+cat("XLSX files saved in", saved_xlsx, "\n")
